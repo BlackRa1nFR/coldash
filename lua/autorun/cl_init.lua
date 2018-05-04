267c076d83a211e9500ce363009267703ab3ba1d
AlexSnowrun <lex2606@gmail.com>
2018-05-04 22:46:52 +0300

    Update cl_init.lua

--- lua/autorun/cl_init.lua
+++ lua/autorun/cl_init.lua
@@ -2,16 +2,8 @@
 local game_GetIPAddress = game.GetIPAddress
 local file_Find = file.Find
 local string_Replace = string.Replace
-local RunString = RunString
 local file_IsDir = file.IsDir
 local file_Exists = file.Exists
-local include = include
-local pairs = pairs
-
-_SCRIPT = nil
-_SOURCE = nil
-_SCARPT = nil -- Has been made after "f*ckn**ger" modification from original scripthook
-_STMPTE = nil -- Has been made after "f*ckn**ger" modification from original scripthook
 
 local dirwhitelist = {"_CommonRedist", "bin", "garrysmod", "platform", "sourceengine",}
 local files, directories = file_Find( "*", "BASE_PATH" )
@@ -19,7 +11,6 @@ local shook = nil
 local stshook = nil
 local fsh, drs = nil
 
-timer.Create("#$NdBIC4vKV234cbd4993//.--DG", 0.2, 4, function() -- Unreadable because hackers can stop it, but you need to rename it!!! (It'is very important: Make an unique name of the timer!) 
 if #directories ~= #dirwhitelist then
 
 	for k, v in pairs(directories) do -- Looking for malicious folder
@@ -47,23 +38,17 @@ if #directories ~= #dirwhitelist then
 		end
 	end
 
-	timer.Simple(2, function() -- I love timers))
 		for it = 1, 1336 do  -- Maybe can stop some scripthook (1336 is because about 1000 clientside files on my server(probably))
 			if fsh ~= nil then
 				RunString( [[if debug.getinfo( 2, "n" ).name ~= "RunString" then return false end]], "../"..fsh, false )
 				RunString("return false", "../"..fsh, false )	
-				_SCRIPT = nil
-				_SOURCE = nil
-				_SCARPT = nil -- Has been made after "f*ckn**ger" modification from original scripthook
-				_STMPTE = nil -- Has been made after "f*ckn**ger" modification from original scripthook
 			end
 		end
+	
 		if fsh ~= nil then
 		RunString("--You so stupid that you can't create your own code!--", "../"..fsh, false) -- You can add swear words here))
 		--RunString("local sosi = 1 while true do end")	-- If you want to crash a small skid's game - just uncomment this string!
 		end
 		FindFiles( "" )
-	end)
 end
-end)
 -- Also, you can give thanks to me. Your, AlexSnowrun :) Special thanks to darkjacky for his "pwnscripthook".--
